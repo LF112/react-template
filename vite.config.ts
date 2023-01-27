de09ac886aaa633a9589f6d137c2fa1212af51af
yume233 <akiayumei@gmail.com>
2023-01-27 14:51:40 +0800

    ✨ 启动自动打开页面，自动刷新

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,29 @@
 import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import svgr from 'vite-plugin-svgr'
 
 //=> Vite config
 export default defineConfig({
 	//=> 插件
-	plugins: [tsconfigPaths(), reactRefresh(), svgr()],
+	plugins: [
+		tsconfigPaths(),
+		react({
+			include: ['*.tsx', '*.ts']
+		}),
+		svgr()
+	],
 	server: {
+		open: true,
 		port: 1024 //=> Dev 监听端口 | '一般在 3000 绑定失败时设置'
 	},
 	build: {
-		sourcemap: false
+		rollupOptions: {
+			output: {
+				chunkFileNames: 'static/js/[name]-[hash].js',
+				entryFileNames: 'static/js/[name]-[hash].js',
+				assetFileNames: 'static/[ext]/[name]-[hash].[ext]'
+			}
+		}
 	}
 })
